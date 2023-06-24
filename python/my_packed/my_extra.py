ed410442a68c62377213ab0d392c9b045b52e69e
fsoylu <fsmorex1@gmail.com>
2023-06-24 23:26:22 +0300

    24062023

---
+++ python/my_packed/my_extra.py
@@ -0,0 +1,9 @@
+def my_len(iter):
+
+    """Bu fonksiyon içerisine yazılan iterable elemanın uzunluğunu döndürür."""
+
+    counter = 0
+    for i in iter:
+        counter += 1
+
+    return counter
---
+++ python/my_packed/my_modül.py
@@ -0,0 +1,4 @@
+def tekleri_ayıkla(* x):
+    """Bu fonksiyon tel sayıların ayıklar"""
+    retuen [i for i in x if i % 2 == 1]
+    
